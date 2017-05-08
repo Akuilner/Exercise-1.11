6ccbcce43f474274636866e434f64418d05a49ce
Akuilner <och5@comcast.net>
2017-05-07 23:31:45 -0400

    Exercise 1.11
    From the textbook

---
+++ src/Exercise1_11.java
@@ -0,0 +1,12 @@
+
+public class Exercise1_11 {
+
+	public static void main(String[] args) {
+		System.out.println("Population after one year " + 2780096.70329 + 312032486);
+		System.out.println("Population after two years " + 2 * 2780096.70329 + 312032486);
+		System.out.println("Population after three years " +3 * 2780096.70329 + 312032486);
+		System.out.println("Population after four years " +4 * 2780096.70329 + 312032486);
+		System.out.println("Population after five years " +5 * 2780096.70329 + 312032486);
+	}
+
+}
